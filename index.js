7b604ec816f23f581522b7a2bb8f525b52469289
Nicolas Dupont <nicolas.dupont@iwecloud.com>
2015-06-24 16:32:03 +0200

    Allow exclusion with regexp

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-
 var assert = require('assert')
 
 module.exports = function (options) {
   assert(options)
   var prefix = options.prefix
+  var regexes = [];
+  if (options.exclude) {
+    options.exclude.forEach(function(excludeRule) {
+      if (excludeRule instanceof RegExp) {
+        regexes.push(excludeRule);
+      }
+    })
+  }
   assert(prefix)
   if (!/\s+$/.test(prefix)) prefix += ' '
   return function (root) {
@@ -11,10 +18,22 @@ module.exports = function (options) {
       // pretty sure this splitting breaks for certain selectors
       var selectors = rule.selector.split(/\s*,\s*/g)
       rule.selector = selectors.map(function (selector) {
-        if (options.exclude && ~options.exclude.indexOf(selector)) {
+        if (!options.exclude) {
+          return prefix + selector;
+        }
+        if (options.exclude.indexOf(selector) !== -1) {
           return selector;
         }
-        return prefix + selector
+        if (regexes.length) {
+
+          var result = prefix + selector;
+          regexes.forEach(function(regex) {
+            if (selector.match(regex)) {
+              result = selector;
+            }
+          })
+          return result;
+        }
       }).join(', ')
     })
   }
